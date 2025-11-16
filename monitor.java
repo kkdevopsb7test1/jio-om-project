53e225c5693d3678d1a7cf231ff6ece06bef6fb3
Pvreddy1 <pvreddy2286@gmail.com>
2025-11-16 14:22:11 +0530

    Update monitor.java

--- monitor.java
+++ monitor.java
@@ -2,9 +2,12 @@ api Version:V1
 kind: Pod
 metadata:
   name: my-pod
+  label:
+  app:nginx
 spec:
   containers:
   - name: nginx container
     image: nginx
     ports:
     - containerPort: 8080
+         
